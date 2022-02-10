da0fa7f4a70e830ba4a560cb14f3cc100099e083
CSI-Fernando-Arias <24-005@sanignacio.pr>
2022-02-10 10:33:31 -0400

    start de hangman

---
+++ Projects/Hangman/Hangman.py
@@ -0,0 +1,43 @@
+import random
+
+
+wordList = ["Clue", "Life", "Jesuita", "Loyola", "Iglesia", "Buenos Aires", "Bogota", "Quito", "loops", "Fajardo", "Orocovis", "Isabela", "Rincon", "Gurabo", "Ponce", "Bayamon", "Cayey", "Monopolio", "string", "list", "float", "Arecibo", "Dorado", "Luquillo"]
+
+steps = ['''
+     |---|
+         |
+         |
+         |
+        ===''', '''
+     |---|
+     O   |
+         |
+         |
+       ===''', '''
+    |---|
+    O   |
+    |   |
+        |
+       ===''', '''
+    |---|
+    O   |
+   /|   |
+        |
+       ===''', '''
+    |---|
+    O   |
+   /|\  |
+        |
+       ===''', '''
+    |---|
+    O   |
+   /|\  |
+   /    |
+       ===''', '''
+    |---|
+    O   |
+   /|\  |
+   / \  |
+       ===''']
+
+    
