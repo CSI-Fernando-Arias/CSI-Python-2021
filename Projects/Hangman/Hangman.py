d875cb75a09645aeafc7c39830dfa99dee3c54fa
CSI-Fernando-Arias <24-005@sanignacio.pr>
2022-02-28 16:53:29 -0400

    hangman

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -1,11 +1,10 @@
-import random
+import random 
 
-#All words that can be guessed in my hangman
-word_list = ["CLUE", "LIFE", "JESUITA", "LOYOLA", "IGLESIA", "BUENOS AIRES", "BOGOTA", "QUITO", "LOOPS", "FAJARDO", "OROCOVIS", "ISABELA", "RINCON", "GURABO", "PONCE", "BAYAMON", "CAYEY", "MONOPOLIO", "STRING", "LIST", "FLOAT", "ARECIBO", "DORADO", "LUQUILLO"]
-#All hangman steps
-def showHangman(tries):
-               
-   steps = ['''
+#All words thats can be used in my hangman
+word_list = ["CLUE", "LIFE", "JESUITA", "LOYOLA", "IGLESIA", "BUENOSAIRES", "BOGOTA", "QUITO", "LOOPS", "FAJARDO", "OROCOVIS", "ISABELA", "RINCON", "GURABO", "PONCE", "BAYAMON", "CAYEY", "MONOPOLIO", "STRING", "LIST", "FLOAT", "ARECIBO", "DORADO", "LUQUILLO"]
+#Every step in hangman
+def display_hangman(tries):
+    steps = ['''
      |---|
          |
          |
@@ -42,68 +41,79 @@ def showHangman(tries):
    / \  |
        ===''']
 
-   return steps[tries]
+    return steps[tries]
 
-#Every letter I cant use or not possible to use
+#This code is to select a random word from my word list
+def getWord(word_list):
+    word = random.choice(word_list)
+    return word.upper()
+#This is a list of the letters I cant use
 incompleteLetters = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
 
-#Selecting a word from my word list
-def get_word():
-   word = random.choice(word_list)
-   return word.upper()
-
-
+#This code is going to print an error if I select a letter thats is in my list of incomplete letters and going to make it a upper case letter.
 def getInput():
    Letter = input("Select letter")
    while(Letter.upper() not in incompleteLetters):
       print("Error")
-
-#Start playing hangman 
+#To start playing
 def play(word):
-   wordSpaces = "-" * len(word) 
-   guessed = ()
-   guessedLetters = []  #This code is for every letter that is guessed 
-   guessedWords = []
-   tries = 6   #Amount of tries
-   print("Vamos a jugar Hangman")
-   print("No use of lowercase, Upper Case only")
-   print(showHangman(tries))#This will show or display the hangman when I run the code
-   print(wordSpaces)#This code will make it show the spaces of each word
+    word_completion = "-" * len(word) #This will show me the amount of spaces the letter chosen has
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6 #amount of tries to guess the word chosen
+    print("Lets play Hangman")
+    print("No use of lowercase, must be Upper Case letters always.")
+    print(display_hangman(tries))#Print the steps so you see how much tries you have left 
+    print(word_completion)#prints the spaces of the letter
+    print("\n")
+    while not guessed and tries > 0:
+        guess = input("Guess the word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("Already used that letter", guess, "!")
+            elif guess not in word:
+                print(guess, "Not the word :(")     
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Yes", guess, "that letter is in the word")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess 
+                word_completion = "".join(word_as_list)
+                if "-" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guessed in guessed_words:
+                print("Already tried that", guess,)
+            elif guess != word:
+                print(guess, "Not the word : (")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Invalid input")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    #I use an if else that tells me either you got the correct word or you ran out of opportunities.
+    if guessed:
+        print("You got the word correct")
+    else: 
+        print("No more tries left. The correct word"+word+" Better luck next time.")
+                
+    
+def main():
+    word = getWord(word_list)
+    play(word)
+    while input("Go again (Y/N)").upper() == "Y":
+        word = getWord(word_list)
+        play(word)
 
-   while not guessed and tries > 0:
-         guess = input("Guess the word:").upper()
-   if len(guess) == 1 and guess.isalpha():
-         if guess in guessedLetters:
-            print("already used that letter", guess)
-         elif guess not in word:
-            print(guess, "Not the word: (")
-            tries -= 1
-            guessedLetters.append(guess)
-         else:
-            print("Yes", guess, "that letter is in the word")
-            guessedLetters.append(guess)
-            word_as_list = list(wordSpaces)
-            indices = [i for i, letter in enumerate(word) if letter == guess]
-            for index in indices:
-               word_as_list[index] = guess
-            word_completion = "".join(word_as_list)
-            if "_" not in word_completion:
-               guessed = True
-   elif len(guess) == len(word) and guess.isalpha():
-         if guessed in guessedWords:
-            print("already tried that", guess, "!")
-         elif guess != word:
-            print(guess, "Not the word: (")
-            tries -= 1
-            guessedLetters.append(guess)
-         else:
-            guessed = True
-            word_completion = word
-   else:
-        
-         if guessed:
-            print("Nice, you got the word correct!! :)")
-         else:
-            print("No more tries left. The correct word"+word+" Better luck next time.")
-
-play(get_word())
+if __name__ == "__main__":
+    main() 
