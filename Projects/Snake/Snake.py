626e22a396c64cef836d0df4b211f982232022f9
CSI-Fernando-Arias <24-005@sanignacio.pr>
2022-03-09 10:14:50 -0400

    first commit of the first code

---
+++ Projects/Snake/Snake.py
@@ -0,0 +1,6 @@
+import pygame
+pygame.init() #initialize the game
+dis=pygame.display.set_mode((400,300)) #created the display of the game.
+pygame.display.update() #This code updates the display of the game. 
+pygame.quit() #uninitialize the game
+quit()
